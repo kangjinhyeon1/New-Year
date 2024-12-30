6655813f6348b8cc41adaf2c0a1ecbddb3f5ee6a
kangjinhyeon1 <kangjin627@gmail.com>
2024-12-30 21:33:28 +0900

    feat: default setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./src/main.tsx"],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./src/main.tsx", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main0: "#FFF6D6",
+        main1: "#FFF2C7",
+        main2: "#FFEEB6",
+        main3: "#FFE07B",
+      },
+      keyframes: {
+        landing: {
+          "0%": { transform: "translate(0, 0)" },
+          "100%": { transform: "translate(0, 100vh)" },
+        },
+      },
+      animation: {
+        landing: "landing 10s linear alternate infinite",
+      },
+    },
   },
   plugins: [],
-}
-
+};
